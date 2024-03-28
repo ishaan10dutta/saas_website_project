cbd0c5c36ffb655c9e1ebf7b35ebe735e2d648c5
Ishaan <ishaan10dutta@gmail.com>
2024-03-28 11:50:33 +0000

    Setup continued.

--- next.config.js
+++ next.config.js
@@ -12,4 +12,4 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig
---
+++ src/app/site/page.tsx
@@ -0,0 +1,17 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import clsx from "clsx";
+import { Check } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return <main className=""></main>;
+}
